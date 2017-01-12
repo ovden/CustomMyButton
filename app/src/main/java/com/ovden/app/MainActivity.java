35ae196c034da4fbb0f2d52e6ab0b22f672b1515
REDNOUT\Денис <ovdengm@gmail.com>
2017-01-12 13:11:08 +0300

    Запустил на SII

--- app/src/main/java/com/ovden/app/MainActivity.java
+++ app/src/main/java/com/ovden/app/MainActivity.java
@@ -1,18 +1,16 @@
 package com.ovden.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends AppCompatActivityActivity {
+
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +48,7 @@ public class MainActivity extends AppCompatActivityActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
+
     public static class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
